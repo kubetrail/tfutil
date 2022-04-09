b8aa3ce92748af75d236dc66d156d7b044fef07f
Saurabh Deoras <sdeoras@gmail.com>
2022-04-09 07:44:14 -0700

    fixed bug in float64 matmul test (#20)
    Signed-off-by: Saurabh Deoras <sdeoras@gmail.com>

--- pkg/tfutil/func_test.go
+++ pkg/tfutil/func_test.go
@@ -9,7 +9,8 @@ import (
 
 func TestMatrixMultiplyFloat64(t *testing.T) {
 	rand.Seed(0)
-	f := func(int) float64 { return rand.Float64() }
+	f := func(int) float64 { return float64(rand.Intn(100)) / 100 }
+
 	x, err := NewTensorFromFunc(f, 3, 4)
 	if err != nil {
 		t.Fatal(err)
@@ -26,7 +27,33 @@ func TestMatrixMultiplyFloat64(t *testing.T) {
 	}
 
 	jb, _ := json.Marshal(z)
-	expected := "{\"type\":\"tensor\",\"tfDataType\":\"Double\",\"goDataType\":\"float64\",\"shape\":[3,2],\"value\":[1.5033243536387162,0.3751283920017592,1.1535534112318055,0.7010293278394667,1.7983002735064946,1.014001106018599]}"
+	expected := "{\"type\":\"tensor\",\"tfDataType\":\"Double\",\"goDataType\":\"float64\",\"shape\":[3,2],\"value\":[0.5977,0.713,1.0464,1.3804,0.9746,1.277]}"
+
+	if !bytes.Equal(jb, []byte(expected)) {
+		t.Fatal("output does not match expected output")
+	}
+}
+
+func TestMatrixMultiplyInt32(t *testing.T) {
+	rand.Seed(0)
+	f := func(int) int32 { return rand.Int31n(100) }
+	x, err := NewTensorFromFunc(f, 3, 4)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	y, err := NewTensorFromFunc(f, 4, 2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	z, err := MatrixMultiply(x, y)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	jb, _ := json.Marshal(z)
+	expected := "{\"type\":\"tensor\",\"tfDataType\":\"Int32\",\"goDataType\":\"int32\",\"shape\":[3,2],\"value\":[5977,7130,10464,13804,9746,12770]}"
 
 	if !bytes.Equal(jb, []byte(expected)) {
 		t.Fatal("output of matrix multiplication does not match expected output")
